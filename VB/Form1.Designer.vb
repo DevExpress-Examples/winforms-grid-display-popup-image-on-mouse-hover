9b7d7630e2e9df91aa5e8d3f81c708f677c49165
DevExpressExampleBot <fakeEmail>
2023-09-11 16:59:42 +0400

    Source auto update [skip ci]

--- VB/Form1.Designer.vb
+++ VB/Form1.Designer.vb
@@ -1,5 +1,7 @@
-﻿Namespace WindowsFormsApplication85
-    Partial Public Class Form1
+Namespace WindowsFormsApplication85
+
+    Partial Class Form1
+
         ''' <summary>
         ''' Required designer variable.
         ''' </summary>
@@ -10,21 +12,21 @@
         ''' </summary>
         ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-            If disposing AndAlso (components IsNot Nothing) Then
-                components.Dispose()
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
             End If
+
             MyBase.Dispose(disposing)
         End Sub
 
-        #Region "Windows Form Designer generated code"
-
+#Region "Windows Form Designer generated code"
         ''' <summary>
         ''' Required method for Designer support - do not modify
         ''' the contents of this method with the code editor.
         ''' </summary>
         Private Sub InitializeComponent()
             Me.components = New System.ComponentModel.Container()
-            Dim resources As New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+            Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(WindowsFormsApplication85.Form1))
             Me.toolTipController1 = New DevExpress.Utils.ToolTipController(Me.components)
             Me.gridControl1 = New DevExpress.XtraGrid.GridControl()
             Me.gridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
@@ -32,13 +34,14 @@
             Me.colCategoryName = New DevExpress.XtraGrid.Columns.GridColumn()
             Me.colDescription = New DevExpress.XtraGrid.Columns.GridColumn()
             Me.imageCollection1 = New DevExpress.Utils.ImageCollection(Me.components)
-            CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(Me.gridView1, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(Me.imageCollection1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.gridControl1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.gridView1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.imageCollection1), System.ComponentModel.ISupportInitialize).BeginInit()
             Me.SuspendLayout()
             ' 
             ' toolTipController1
             ' 
+            AddHandler Me.toolTipController1.GetActiveObjectInfo, New DevExpress.Utils.ToolTipControllerGetActiveObjectInfoEventHandler(AddressOf Me.toolTipController1_GetActiveObjectInfo)
             ' 
             ' gridControl1
             ' 
@@ -49,11 +52,11 @@
             Me.gridControl1.Size = New System.Drawing.Size(463, 315)
             Me.gridControl1.TabIndex = 0
             Me.gridControl1.ToolTipController = Me.toolTipController1
-            Me.gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() { Me.gridView1})
+            Me.gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.gridView1})
             ' 
             ' gridView1
             ' 
-            Me.gridView1.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() { Me.colCategoryID, Me.colCategoryName, Me.colDescription})
+            Me.gridView1.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.colCategoryID, Me.colCategoryName, Me.colDescription})
             Me.gridView1.GridControl = Me.gridControl1
             Me.gridView1.Name = "gridView1"
             ' 
@@ -84,7 +87,7 @@
             ' imageCollection1
             ' 
             Me.imageCollection1.ImageSize = New System.Drawing.Size(128, 128)
-            Me.imageCollection1.ImageStream = (CType(resources.GetObject("imageCollection1.ImageStream"), DevExpress.Utils.ImageCollectionStreamer))
+            Me.imageCollection1.ImageStream = CType((resources.GetObject("imageCollection1.ImageStream")), DevExpress.Utils.ImageCollectionStreamer)
             Me.imageCollection1.Images.SetKeyName(0, "1.png")
             Me.imageCollection1.Images.SetKeyName(1, "2.png")
             Me.imageCollection1.Images.SetKeyName(2, "3.png")
@@ -99,22 +102,25 @@
             Me.Controls.Add(Me.gridControl1)
             Me.Name = "Form1"
             Me.Text = "Form1"
-            CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(Me.gridView1, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(Me.imageCollection1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.gridControl1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.gridView1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.imageCollection1), System.ComponentModel.ISupportInitialize).EndInit()
             Me.ResumeLayout(False)
-
         End Sub
 
-        #End Region
+#End Region
+        Private toolTipController1 As DevExpress.Utils.ToolTipController
 
-        Private WithEvents toolTipController1 As DevExpress.Utils.ToolTipController
         Private gridControl1 As DevExpress.XtraGrid.GridControl
+
         Private gridView1 As DevExpress.XtraGrid.Views.Grid.GridView
+
         Private colCategoryID As DevExpress.XtraGrid.Columns.GridColumn
+
         Private colCategoryName As DevExpress.XtraGrid.Columns.GridColumn
+
         Private colDescription As DevExpress.XtraGrid.Columns.GridColumn
+
         Private imageCollection1 As DevExpress.Utils.ImageCollection
     End Class
 End Namespace
-
--- VB/Form1.vb
+++ VB/Form1.vb
@@ -1,4 +1,4 @@
-﻿Imports DevExpress.Utils
+Imports DevExpress.Utils
 Imports DevExpress.XtraEditors
 Imports DevExpress.XtraGrid.Views.Grid
 Imports DevExpress.XtraGrid.Views.Grid.ViewInfo
@@ -6,44 +6,37 @@ Imports System.ComponentModel
 Imports System.Drawing
 
 Namespace WindowsFormsApplication85
-    Partial Public Class Form1
+
+    Public Partial Class Form1
         Inherits XtraForm
 
         Public Sub New()
             InitializeComponent()
-
             Dim list = New BindingList(Of Item)()
-            For i As Integer = 0 To 4
-                list.Add(New Item() With { _
-                    .CategoryID = i, _
-                    .CategoryName = "Test name" & i, _
-                    .Description = "Description" & i, _
-                    .Picture = imageCollection1.Images(i) _
-                })
-            Next i
+            For i As Integer = 0 To 5 - 1
+                list.Add(New Item() With {.CategoryID = i, .CategoryName = "Test name" & i, .Description = "Description" & i, .Picture = imageCollection1.Images(i)})
+            Next
+
             gridControl1.DataSource = list
         End Sub
 
-        Private Sub toolTipController1_GetActiveObjectInfo(ByVal sender As Object, ByVal e As ToolTipControllerGetActiveObjectInfoEventArgs) Handles toolTipController1.GetActiveObjectInfo
-            If e.SelectedControl IsNot gridControl1 Then
-                Return
-            End If
+        Private Sub toolTipController1_GetActiveObjectInfo(ByVal sender As Object, ByVal e As ToolTipControllerGetActiveObjectInfoEventArgs)
+            If e.SelectedControl IsNot gridControl1 Then Return
             Dim info As ToolTipControlInfo = Nothing
-            Dim sTooltip1 As New SuperToolTip()
+            Dim sTooltip1 As SuperToolTip = New SuperToolTip()
             Try
                 Dim view As GridView = TryCast(gridControl1.GetViewAt(e.ControlMousePosition), GridView)
-                If view Is Nothing Then
-                    Return
-                End If
+                If view Is Nothing Then Return
                 Dim hi As GridHitInfo = view.CalcHitInfo(e.ControlMousePosition)
                 If hi.HitTest = GridHitTest.RowIndicator Then
                     info = New ToolTipControlInfo(GridHitTest.RowIndicator.ToString() & hi.RowHandle.ToString(), "Row Handle: " & hi.RowHandle.ToString())
-                    Dim titleItem1 As New ToolTipTitleItem()
+                    Dim titleItem1 As ToolTipTitleItem = New ToolTipTitleItem()
                     Dim im As Image = TryCast(view.GetRowCellValue(hi.RowHandle, "Picture"), Image)
-                    Dim item1 As New ToolTipItem()
+                    Dim item1 As ToolTipItem = New ToolTipItem()
                     item1.Image = im
                     sTooltip1.Items.Add(item1)
                 End If
+
                 info = New ToolTipControlInfo(hi.HitTest, "")
                 info.SuperTip = sTooltip1
             Finally
@@ -51,10 +44,15 @@ Namespace WindowsFormsApplication85
             End Try
         End Sub
     End Class
+
     Public Class Item
-        Public Property CategoryID() As Integer
-        Public Property CategoryName() As String
-        Public Property Description() As String
-        Public Property Picture() As Image
+
+        Public Property CategoryID As Integer
+
+        Public Property CategoryName As String
+
+        Public Property Description As String
+
+        Public Property Picture As Image
     End Class
 End Namespace
